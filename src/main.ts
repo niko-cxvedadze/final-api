16218a6abdbb3c6be2241eb2057addf8fe71c3e2
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2023-10-26 21:27:22 +0400

    feature(validation-pipe)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      exceptionFactory: (errors) => {
+        const result = errors.map((error) => ({
+          property: error.property,
+          message: error.constraints[Object.keys(error.constraints)[0]],
+        }));
+        return new BadRequestException(result);
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
