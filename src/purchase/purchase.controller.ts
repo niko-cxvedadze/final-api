6075eafc48b01b6bfc02106f176dad09cfd9db28
RomanLetodiani <roman.letodiani03@gmail.com>
2024-04-28 13:03:07 +0400

    Update purchase.controller.ts

--- src/purchase/purchase.controller.ts
+++ src/purchase/purchase.controller.ts
@@ -1,17 +1,32 @@
-import { Controller, Post, Get, Body, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Param,
+  Delete,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
 import { PurchaseService } from './purchase.service';
 import { CreatePurchaseDto } from './dtos/purchase.dto';
 import { Purchase } from './purchase.entity';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('purchases')
 export class PurchaseController {
   constructor(private readonly purchaseService: PurchaseService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async createPurchase(
     @Body() createPurchaseDto: CreatePurchaseDto,
+    @Req() req,
   ): Promise<Purchase> {
-    return await this.purchaseService.createPurchase(createPurchaseDto);
+    return await this.purchaseService.createPurchase(
+      createPurchaseDto,
+      req.user_id,
+    );
   }
 
   @Get()
